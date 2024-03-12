27b1daff3da146f0a4522e702c4009c606f42426
cassaram <michael@cassara.tech>
2024-03-11 22:16:28 -0400

    Finish setters / getters for config values by string path

--- backend/config/showConfig.go
+++ backend/config/showConfig.go
@@ -68,5 +68,36 @@ func (c *ShowConfig) GetValue(path []string) (string, error) {
 }
 
 func (c *ShowConfig) SetValue(path []string, value string) error {
-
+	switch path[0] {
+	case "name":
+		c.Name = value
+		return nil
+	case "filename":
+		c.FileName = value
+		return nil
+	case "channel_cfgs":
+		id, err := strconv.Atoi(path[1])
+		if err != nil {
+			return err
+		}
+		return c.ChannelCfgs[id].SetValue(path[2:], value)
+	case "bus_cfgs":
+		id, err := strconv.Atoi(path[1])
+		if err != nil {
+			return err
+		}
+		return c.BusCfgs[id].SetValue(path[2:], value)
+	case "crosspoint_cfgs":
+		channel_id, err := strconv.Atoi(path[1])
+		if err != nil {
+			return err
+		}
+		bus_id, err := strconv.Atoi(path[2])
+		if err != nil {
+			return err
+		}
+		return c.CrosspointCfgs[channel_id][bus_id].SetValue(path[3:], value)
+	default:
+		return fmt.Errorf("encountered unexpected path variable %s", path[0])
+	}
 }
